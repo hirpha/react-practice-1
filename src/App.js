ad72989c8835b8e6880a772741ba7298a4aeb066
Hirpha Fayisa <hirphafayisa88@gmail.com>
2024-12-16 22:43:04 +0300

    read me updated

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles/App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav-bar">
+        <div>Logo</div>
+        <div className="rigth-nav-bar">
+          <div className="Get-started">Get started</div>
+          <div className="sign-in">Sign in</div>
+        </div>
+      </div>
+
+      <div className="body-header">Connect with your friends easly</div>
+    </div>
+  );
+}
+
+export default App;
